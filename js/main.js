b244ec74cf382a0b9a9fa1818d0a5f08fee6c8f1
Jesse Hall <jessehall3@gmail.com>
2016-11-19 19:06:14 -0800

    got rid of broswer sniff and polify as didn't finish the job, more work is required

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 * Full license: https://github.com/ableplayer/ableplayer/blob/master/LICENSE
 */
 
-// (function($, key, window, document){
+(function($, key, window, document){
 
 var model = {
     defaultAudioList : ["quijote_chap_1_spanish.ogg",
@@ -946,19 +946,7 @@ var controlsView ={
 };
 
 $(document).ready(function($) {
-    // quick fix for IE promises support
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf("MSIE ");
-    if (msie > 0) // If Internet Explorer, return version number
-    {
-        $.getScript( "https://cdnjs.cloudflare.com/ajax/libs/bluebird/3.3.4/bluebird.min.js", function( data, textStatus, jqxhr ) {
-          console.log( data ); // Data returned
-          console.log( textStatus ); // Success
-          console.log( jqxhr.status ); // 200
-          console.log( "Load was performed." );
-        })
-    }
-  controller.init();
+    controller.init();
 });
 
-// }(window.jQuery, window.key, window, document));
+}(window.jQuery, window.key, window, document));
